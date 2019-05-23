691660c34468ab118d68a85e864cb85dd2b21432
Jaimek89 <jaimek89@gmail.com>
2019-05-23 10:47:30 +0100

    removed node_modules

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
 
 // Initializations
 const app = express()
 
 // Setting
 app.set('port', 3000)
+
+// Middlewares
+app.use(morgan())
 
 // Start the server
 app.listen(app.get('port'), () => {
